796b25dd836b23ced44edacec002fbc82546aec9
dltmddus1998 <dltmddus1998@naver.com>
2022-06-16 22:30:04 +0900

    ADD: 라우터 및 컨트롤러 구성

--- router/recruit_notice.js
+++ router/recruit_notice.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import * as recruitController from '../controller/recruit.js';
 
 const router = express.Router();
 
-router.post('/', recruitController.registerRecruitNotice);
+export default function recruitRouter(recruitController) {
+    router.post('/', recruitController.registerRecruitNotice);
 
-router.patch('/:notice_id', recruitController.modifyRecruitNotice);
+    router.patch('/:notice_id', recruitController.modifyRecruitNotice);
 
-router.delete('/:notice_id', recruitController.removeRecruitNotice);
+    router.delete('/:notice_id', recruitController.removeRecruitNotice);
 
-router.get('/:notice_id', recruitController.getDetailNotice);
+    router.get('/:notice_id', recruitController.getDetailNotice);
 
-router.get('/', recruitController.getAllNotices);
-
-export default router;
+    router.get('/', recruitController.getAllNotices);
+    return router;
+}
